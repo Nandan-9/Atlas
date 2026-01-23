99d135c7645203499b88e04799e4443819c7f220
a.d.devnandan <a.d.devnandan@gmail.com>
2026-01-24 00:50:34 +0530

    added the user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class User(models.Model):
+
+    ROLE_CHOICE = [
+        ('team_lead','Team Lead'),
+        ('mananger', 'Manager'),
+        ('dev','Developer'),
+        ('intern',"Intern")
+    ]
+
+    username = models.CharField(max_length=100,unique=True)
+    email_id = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICE)
+    password = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.email_id
+
+
+
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import User
 
-# Create your views here.
+@csrf_exempt
+def users_view(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+
+        user = User.objects.create(
+            username=data["username"],
+            email_id=data["email_id"],
+            role=data["role"],
+            password=data["password"],
+        )
+
+        return JsonResponse({
+            "id": user.id,
+            "message": "User created successfully"
+        }, status=201)
+
+    if request.method == "GET":
+        users = User.objects.all().values(
+            "id", "username", "email_id", "role"
+        )
+        return JsonResponse(list(users), safe=False)
